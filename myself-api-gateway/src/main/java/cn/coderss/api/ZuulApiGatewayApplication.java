f22013b092e5efc6cd99695136b913bcad5650ab
shenwei <admin@coderss.cn>
2017-12-05 22:18:06 +0800

    git ignore

--- myself-api-gateway/src/main/java/cn/coderss/api/ZuulApiGatewayApplication.java
+++ myself-api-gateway/src/main/java/cn/coderss/api/ZuulApiGatewayApplication.java
@@ -1,6 +1,9 @@
 package cn.coderss.api;
 
+import cn.coderss.common.utils.TestA;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -17,7 +20,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 @EnableZuulProxy
 //@EnableApolloConfig(value = "application", order = 10)
-public class ZuulApiGatewayApplication extends WebMvcConfigurerAdapter{
+public class ZuulApiGatewayApplication extends WebMvcConfigurerAdapter implements CommandLineRunner{
+    @Autowired
+    TestA testA;
+
     public static void main(String[] args) {
         SpringApplication.run(ZuulApiGatewayApplication.class, args);
     }
@@ -26,5 +32,10 @@ public class ZuulApiGatewayApplication extends WebMvcConfigurerAdapter{
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/index").setViewName("index");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        testA.test();
     }
 }
